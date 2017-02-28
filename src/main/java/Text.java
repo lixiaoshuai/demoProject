77bf6810a4a3155885e42d8f4a52a39f006e2fc7
sxl <894153062@qq.com>
2017-02-28 23:17:29 +0800

    1

---
+++ src/main/java/Text.java
@@ -0,0 +1,12 @@
+/**
+ * Created by sxl on 2017/2/23.
+ */
+public class Text {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println((Integer)129==(Integer)129);
+    }
+}
